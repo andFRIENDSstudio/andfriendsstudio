cf9acc6bcdc1244aaaa42612cd63953260d77831
Mason Thompson <mfthompson99@gmail.com>
2025-10-19 11:37:41 -0700

    Add vercel adapter for SSR

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,8 @@
+// @ts-nocheck
 import { defineConfig } from "tinacms";
 
 export default defineConfig({
-  branch: "main", // Change to "master" if that's your branch name
+  branch: "master", // Change to "master" if that's your branch name
   
   clientId: 38f5cc0a-f76d-42d8-99c2-9f87deeaf2f3, // We'll get this from Tina Cloud in a sec
   token: 082efe1a861019acf25a8c6348462be9514c305a, // We'll get this too
